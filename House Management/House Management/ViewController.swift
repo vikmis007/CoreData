f5e9a863703c886fb39b694b6d4a7971a9f309a2
VIkas Mishra <vikasmishra.sastra@gmail.com>
2019-04-10 11:15:16 +0530

    Project setup Created

---
+++ House Management/House Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  House Management
+//
+//  Created by Vikasmishra on 10/04/19.
+//  Copyright © 2019 Exercise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ House Management/House ManagementTests/House_ManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  House_ManagementTests.swift
+//  House ManagementTests
+//
+//  Created by Vikasmishra on 10/04/19.
+//  Copyright © 2019 Exercise. All rights reserved.
+//
+
+import XCTest
+@testable import House_Management
+
+class House_ManagementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
